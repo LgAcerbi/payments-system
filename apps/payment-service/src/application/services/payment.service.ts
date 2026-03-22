a6204c6d8b76f0801cc3aaebc86e409543ffdd7b
lgacerbi <lgacerbi@gmail.com>
2026-03-21 22:29:41 -0300

    feat: enhance PaymentService to integrate PaymentGateway for creating payment intents, improving payment processing flow

--- apps/payment-service/src/application/services/payment.service.ts
+++ apps/payment-service/src/application/services/payment.service.ts
@@ -1,10 +1,28 @@
-import type { PaymentRepository } from '..';
-import type { Payment } from '../../domain';
+import type { PaymentRepository, PaymentGateway } from '..';
+
+import { Payment } from '../../domain';
 
 class PaymentService {
-    constructor(private readonly paymentRepository: PaymentRepository) {}
+    constructor(private readonly paymentRepository: PaymentRepository, private readonly paymentGateway: PaymentGateway) {}
 
-    async createPayment(payment: Payment): Promise<Payment> {
+    async createPayment(paymentData: Omit<Payment, 'id' | 'intentId' | 'createdAt' | 'updatedAt'>): Promise<Payment> {
+        const paymentIntent = await this.paymentGateway.createPaymentIntent(paymentData.amount, paymentData.currency);
+
+        const payment = new Payment(
+            {
+                id: paymentIntent.id,
+                intentId: paymentIntent.id,
+                status: 'INITIATED',
+                amount: paymentData.amount,
+                currency: paymentData.currency,
+                orderId: paymentData.orderId,
+                method: paymentData.method,
+                methodOptions: paymentData.methodOptions,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            }
+        );
+
         return this.paymentRepository.createPayment(payment);
     }
 
