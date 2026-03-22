4268ec386df8d9182c390a8dcb18997bc5be77ea
lgacerbi <lgacerbi@gmail.com>
2026-03-21 22:29:27 -0300

    refactor: restructure Payment class to use a constructor with object destructuring and introduce PaymentStatus type for improved readability and maintainability

--- apps/payment-service/src/domain/entities/payment.ts
+++ apps/payment-service/src/domain/entities/payment.ts
@@ -1,16 +1,56 @@
+type PaymentStatus =
+    | 'INITIATED'
+    | 'PROCESSING'
+    | 'SUCCEEDED'
+    | 'FAILED'
+    | 'CANCELED';
+
 class Payment {
-    constructor(
-        public readonly id: string,
-        public readonly amount: number,
-        public readonly currency: string,
-        public readonly status: 'INITIATED' | 'PROCESSING' | 'SUCCEEDED' | 'FAILED' | 'CANCELED',
-        public readonly orderId: string,
-        public readonly intentId: string,
-        public readonly method: string,
-        public readonly methodOptions: Record<string, unknown>,
-        public readonly createdAt: Date,
-        public readonly updatedAt: Date,
-    ) {}
+    public readonly id: string;
+    public readonly amount: number;
+    public readonly currency: string;
+    public readonly status: PaymentStatus;
+    public readonly orderId: string;
+    public readonly intentId: string;
+    public readonly method: string;
+    public readonly methodOptions: Record<string, unknown>;
+    public readonly createdAt: Date;
+    public readonly updatedAt: Date;
+
+    constructor({
+        id,
+        amount,
+        currency,
+        status,
+        orderId,
+        intentId,
+        method,
+        methodOptions,
+        createdAt,
+        updatedAt,
+    }: {
+        id: string;
+        amount: number;
+        currency: string;
+        status: PaymentStatus;
+        orderId: string;
+        intentId: string;
+        method: string;
+        methodOptions: Record<string, unknown>;
+        createdAt: Date;
+        updatedAt: Date;
+    }) {
+        this.id = id;
+        this.amount = amount;
+        this.currency = currency;
+        this.status = status;
+        this.orderId = orderId;
+        this.intentId = intentId;
+        this.method = method;
+        this.methodOptions = methodOptions;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
 
 export default Payment;
