b2b5c8c1610e65361cad29bfc066f497816083e5
lgacerbi <lgacerbi@gmail.com>
2026-03-22 09:46:10 -0300

    feat: implement PostgresPaymentRepository and payment schema for handling payment operations in PostgreSQL

---
+++ apps/payment-service/src/adapters/postgres/payment.repository.ts
@@ -0,0 +1,103 @@
+import type { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import type { PaymentDbSchema } from './payment.schema';
+
+import { eq } from 'drizzle-orm';
+import { Payment } from '../../domain';
+import { paymentDbSchema } from './payment.schema';
+import { PaymentRepository } from '../../application';
+
+class PostgresPaymentRepository implements PaymentRepository {
+    constructor(private readonly db: NodePgDatabase<PaymentDbSchema>) {}
+
+    async createPayment(payment: Payment): Promise<Payment> {
+        await this.db
+            .insert(paymentDbSchema.payments)
+            .values(payment)
+            .returning();
+
+        return payment;
+    }
+
+    async getPaymentById(id: string): Promise<Payment> {
+        const [result] = await this.db.select().from(paymentDbSchema.payments).where(eq(paymentDbSchema.payments.id, id));
+        
+        const payment = new Payment(
+            {
+                id: result.id,
+                amount: result.amount,
+                description: result.description,
+                amountRefunded: result.amountRefunded,
+                currency: result.currency,
+                status: result.status,
+                orderId: result.orderId,
+                method: result.method,
+                provider: result.provider,
+                providerPaymentId: result.providerPaymentId,
+                providerData: result.providerData,
+                idempotencyKey: result.idempotencyKey,
+                createdAt: result.createdAt,
+                updatedAt: result.updatedAt,
+            }
+        );
+
+        return payment;
+    }
+
+    async getPaymentByProviderId(providerId: string): Promise<Payment> {
+        const [result] = await this.db.select().from(paymentDbSchema.payments).where(eq(paymentDbSchema.payments.providerPaymentId, providerId));
+        
+        const payment = new Payment(
+            {
+                id: result.id,
+                idempotencyKey: result.idempotencyKey,
+                amount: result.amount,
+                description: result.description,
+                amountRefunded: result.amountRefunded,
+                currency: result.currency,
+                status: result.status,
+                orderId: result.orderId,
+                method: result.method,
+                provider: result.provider,
+                providerPaymentId: result.providerPaymentId,
+                providerData: result.providerData,
+                createdAt: result.createdAt,
+                updatedAt: result.updatedAt,
+            }
+        );
+
+        return payment;
+    }
+
+    async getPaymentByOrderId(orderId: string): Promise<Payment> {
+        const [result] = await this.db.select().from(paymentDbSchema.payments).where(eq(paymentDbSchema.payments.orderId, orderId));
+        
+        const payment = new Payment(
+            {
+                id: result.id,
+                idempotencyKey: result.idempotencyKey,
+                amount: result.amount,
+                description: result.description,
+                amountRefunded: result.amountRefunded,
+                currency: result.currency,
+                status: result.status,
+                orderId: result.orderId,
+                method: result.method,
+                provider: result.provider,
+                providerPaymentId: result.providerPaymentId,
+                providerData: result.providerData,
+                createdAt: result.createdAt,
+                updatedAt: result.updatedAt,
+            }
+        );
+
+        return payment;
+    }
+
+    async confirmPaymentIntent(paymentId: string): Promise<void> {
+        await this.db.update(paymentDbSchema.payments).set({ status: 'processing' }).where(eq(paymentDbSchema.payments.id, paymentId));
+
+    }
+}
+
+export default PostgresPaymentRepository;
+export { PostgresPaymentRepository };
---
+++ apps/payment-service/src/adapters/postgres/payment.schema.ts
@@ -0,0 +1,40 @@
+import { pgEnum, pgTable, uuid, varchar, timestamp, jsonb, bigint } from "drizzle-orm/pg-core"
+
+const paymentStatusEnum = pgEnum("payment_status", [
+    "initiated",
+    "processing",
+    "succeeded",
+    "failed",
+    "canceled",
+])
+
+const paymentProviderEnum = pgEnum("payment_provider", [
+    "stripe",
+])
+
+const postgresPaymentSchema = pgTable("payments", {
+    id: uuid("id").primaryKey(),
+    amount: bigint("amount", { mode: "number" }).notNull(),
+    amountRefunded: bigint("amount_refunded", { mode: "number" }),
+    description: varchar("description", { length: 255 }),
+    currency: varchar("currency", { length: 3 }).notNull(),
+    status: paymentStatusEnum("status").notNull(),
+    orderId: uuid("order_id").notNull(),
+    method: varchar("method", { length: 32 }).notNull(),
+    provider: paymentProviderEnum("provider").notNull(),
+    providerPaymentId: varchar("provider_payment_id", { length: 255 }).notNull(),
+    providerData: jsonb("provider_data"),
+    idempotencyKey: varchar("idempotency_key", { length: 32 }).notNull(),
+    createdAt: timestamp("created_at").notNull(),
+    updatedAt: timestamp("updated_at").notNull(),
+})
+
+const paymentDbSchema = {
+    payments: postgresPaymentSchema,
+}
+
+type PaymentDbSchema = typeof paymentDbSchema
+
+export default postgresPaymentSchema
+export { postgresPaymentSchema, paymentDbSchema, paymentStatusEnum }
+export type { PaymentDbSchema }
