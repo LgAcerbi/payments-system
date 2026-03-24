037b320aa53700bdccd922a1414a3c6c9f3452af
lgacerbi <lgacerbi@gmail.com>
2026-03-23 23:25:58 -0300

    feat: add updatePaymentEventPaymentId method to PaymentEventRepository for updating payment event IDs

--- apps/payment-service/src/application/ports/payment-event.repository.ts
+++ apps/payment-service/src/application/ports/payment-event.repository.ts
@@ -2,6 +2,7 @@ import type { PaymentEvent } from "../../domain/entities/payment-event";
 
 interface PaymentEventRepository {
     createPaymentEvent(paymentEvent: PaymentEvent): Promise<PaymentEvent>;
+    updatePaymentEventPaymentId(id: PaymentEvent['id'], paymentId: PaymentEvent['paymentId']): Promise<void>;
     findPaymentEventByIdempotencyKey(idempotencyKey: PaymentEvent['idempotencyKey']): Promise<PaymentEvent>;
 }
 
