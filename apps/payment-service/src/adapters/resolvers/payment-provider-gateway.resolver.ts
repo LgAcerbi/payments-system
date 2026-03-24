f7ab0991354cfb7fe29ef159e107c614b01a6439
lgacerbi <lgacerbi@gmail.com>
2026-03-24 11:25:32 -0300

    refactor: simplify constructor in PaymentProviderGatewayResolverAdapter for improved readability

--- apps/payment-service/src/adapters/resolvers/payment-provider-gateway.resolver.ts
+++ apps/payment-service/src/adapters/resolvers/payment-provider-gateway.resolver.ts
@@ -5,9 +5,7 @@ import type { Payment } from "../../domain";
 import { ValidationError } from "@workspace/errors";
 
 class PaymentProviderGatewayResolverAdapter implements PaymentProviderGatewayResolver {
-    constructor(private readonly paymentProviderGateways: Map<Payment['provider'], PaymentProviderGateway>) {
-        this.paymentProviderGateways = new Map<Payment['provider'], PaymentProviderGateway>();
-    }
+    constructor(private readonly paymentProviderGateways: Map<Payment['provider'], PaymentProviderGateway>) {}
 
     public resolve(provider: Payment['provider']): PaymentProviderGateway {
         const paymentProviderGateway = this.paymentProviderGateways.get(provider);
