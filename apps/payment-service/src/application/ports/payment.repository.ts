87123c23dbe111f9be0c122fa3a5b524391e2820
lgacerbi <lgacerbi@gmail.com>
2026-03-23 23:11:32 -0300

    refactor: update PaymentRepository interface to improve method signatures and add new methods for updating payment status

--- apps/payment-service/src/application/ports/payment.repository.ts
+++ apps/payment-service/src/application/ports/payment.repository.ts
@@ -2,10 +2,12 @@ import type { Payment } from "../../domain";
 
 interface PaymentRepository {
     createPayment(payment: Payment): Promise<Payment>;
-    getPaymentById(id: string): Promise<Payment>;
-    getPaymentByProviderId(providerId: string): Promise<Payment>;
-    getPaymentByOrderId(orderId: string): Promise<Payment>;
-    confirmPaymentIntent(paymentId: string): Promise<void>;
+    getPaymentById(id: Payment['id']): Promise<Payment>;
+    getPaymentByProviderPaymentId(providerPaymentId: Payment['providerPaymentId'], provider: Payment['provider']): Promise<Payment>;
+    getPaymentByOrderId(orderId: Payment['orderId']): Promise<Payment>;
+    confirmPaymentIntent(paymentId: Payment['id']): Promise<void>;
+    updatePaymentStatusById(paymentId: Payment['id'], status: Payment['status']): Promise<void>;
+    updatePaymentStatusByProviderPaymentId(providerPaymentId: Payment['providerPaymentId'], status: Payment['status'], provider: Payment['provider']): Promise<void>;
 }
 
 export default PaymentRepository;
