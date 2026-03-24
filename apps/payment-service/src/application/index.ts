274cc3d8f2598f6eaa9c0f52b6b045ab7c4820e9
lgacerbi <lgacerbi@gmail.com>
2026-03-24 11:25:41 -0300

    refactor: add new resolvers for payment event mapping and provider gateway to enhance modularity

--- apps/payment-service/src/application/index.ts
+++ apps/payment-service/src/application/index.ts
@@ -4,3 +4,5 @@ export * from "./ports/payment.repository";
 export * from "./ports/payment-event.repository";
 export * from "./ports/payment-provider.gateway";
 export * from "./ports/payment-event.mapper";
+export * from "./ports/payment-provider-gateway.resolver";
+export * from "./ports/payment-event-mapper.resolver";
