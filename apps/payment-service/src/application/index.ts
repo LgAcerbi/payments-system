24332d89dd06b5ec30131ec4c24e7f1d4a3b70f7
lgacerbi <lgacerbi@gmail.com>
2026-03-23 23:58:33 -0300

    chore: add payment-event.mapper export to index for improved module accessibility

--- apps/payment-service/src/application/index.ts
+++ apps/payment-service/src/application/index.ts
@@ -3,3 +3,4 @@ export * from "./services/payment-event.service";
 export * from "./ports/payment.repository";
 export * from "./ports/payment-event.repository";
 export * from "./ports/payment.gateway";
+export * from "./ports/payment-event.mapper";
