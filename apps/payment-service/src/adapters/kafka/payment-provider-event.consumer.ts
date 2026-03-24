47a4ededb3da31076544cb40fc0a853f6219477a
lgacerbi <lgacerbi@gmail.com>
2026-03-23 22:17:30 -0300

    feat: implement PaymentProviderEventConsumer to handle Kafka messages for payment provider events with validation and processing logic

---
+++ apps/payment-service/src/adapters/kafka/payment-provider-event.consumer.ts
@@ -0,0 +1,144 @@
+import type { Consumer } from 'kafkajs';
+import type { PaymentService } from '../../application';
+
+import { logger } from '@workspace/logger';
+import { ValidationError } from '@workspace/errors';
+import { paymentProviderEventDtoSchema } from '@workspace/payment';
+import { StripePaymentEventMapper } from '../stripe/stripe-payment-event.mapper';
+
+const eventProviderMapperFunctionMap = new Map([
+    ['stripe', StripePaymentEventMapper.toPaymentProviderEvent],
+]);
+
+const processableEvents = [
+    'payment-created',
+    'payment-succeeded',
+    'payment-processing',
+    'payment-failed',
+    'payment-canceled',
+];
+
+class PaymentProviderEventConsumer {
+    constructor(
+        private readonly kafkaConsumer: Consumer,
+        private readonly paymentService: PaymentService,
+    ) {}
+
+    private buildKafkaMessageId(
+        topic: string,
+        partition: number,
+        offset: string,
+    ): string {
+        return `${topic}:${partition}:${offset}`;
+    }
+
+    public async startConsume(): Promise<void> {
+        await this.kafkaConsumer.run({
+            eachMessage: async ({ topic, partition, message }) => {
+                try {
+                    const kafkaMessageId = this.buildKafkaMessageId(
+                        topic,
+                        partition,
+                        message.offset,
+                    );
+
+                    logger.info(
+                        {
+                            kafkaMessageId,
+                        },
+                        'Consuming payment provider event message',
+                    );
+
+                    const eventMessage = message.value?.toString();
+
+                    if (!eventMessage) {
+                        throw new ValidationError(
+                            'Empty payment provider event-message',
+                        );
+                    }
+
+                    let jsonParsedEventMessage: unknown;
+
+                    try {
+                        jsonParsedEventMessage = JSON.parse(eventMessage);
+                    } catch (error) {
+                        throw new ValidationError(
+                            `Invalid payment provider event-message: ${eventMessage} is not a valid JSON`,
+                        );
+                    }
+
+                    const parsedEvent =
+                        paymentProviderEventDtoSchema.safeParse(jsonParsedEventMessage);
+
+                    if (!parsedEvent.success) {
+                        throw new ValidationError(
+                            `Invalid payment provider event-message: ${parsedEvent.error.message} for message: ${eventMessage}`,
+                        );
+                    }
+
+                    const eventProviderMapperFunction =
+                        eventProviderMapperFunctionMap.get(
+                            parsedEvent.data.provider,
+                        );
+
+                    if (!eventProviderMapperFunction) {
+                        throw new ValidationError(
+                            `Invalid payment provider: ${parsedEvent.data.provider}`,
+                        );
+                    }
+
+                    const providerPaymentEvent = eventProviderMapperFunction(
+                        parsedEvent.data,
+                    );
+
+                    if (
+                        !processableEvents.includes(providerPaymentEvent.event)
+                    ) {
+                        logger.info(
+                            {
+                                kafkaMessageId,
+                            },
+                            `Skipping non-processable payment provider event: ${providerPaymentEvent.event}`,
+                        );
+                        return;
+                    }
+
+                    logger.info(
+                        {
+                            kafkaMessageId,
+                        },
+                        'Validates payment provider event message',
+                    );
+
+                    await this.paymentService.updatePaymentStatusByProviderPaymentId(
+                        providerPaymentEvent.providerPaymentId,
+                        providerPaymentEvent.status,
+                    );
+
+                    logger.info(
+                        {
+                            kafkaMessageId,
+                        },
+                        'Processed payment provider event message',
+                    );
+                } catch (error) {
+                    if (error instanceof ValidationError) {
+                        logger.error(
+                            { topic, partition, offset: message.offset },
+                            `Validation error consuming payment provider event: ${error.message}`,
+                        );
+                        return;
+                    }
+
+                    logger.error(
+                        { topic, partition, offset: message.offset },
+                        `Unknown error consuming payment provider event`,
+                    );
+                }
+            },
+        });
+    }
+}
+
+export default PaymentProviderEventConsumer;
+export { PaymentProviderEventConsumer };
