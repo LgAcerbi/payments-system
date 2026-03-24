040663059d19c2dc89b14216fa204aa77d5f1dae
lgacerbi <lgacerbi@gmail.com>
2026-03-23 23:12:15 -0300

    feat: export PaymentEvent service and repository from application index for improved event handling

--- apps/payment-service/src/application/index.ts
+++ apps/payment-service/src/application/index.ts
@@ -1,3 +1,5 @@
 export * from "./services/payment.service";
+export * from "./services/payment-event.service";
 export * from "./ports/payment.repository";
+export * from "./ports/payment-event.repository";
 export * from "./ports/payment.gateway";
