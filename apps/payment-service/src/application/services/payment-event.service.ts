5abcfbd42993fda3aefd22f47b0175e5c45b4975
lgacerbi <lgacerbi@gmail.com>
2026-03-23 23:14:21 -0300

    feat: implement PaymentEventService for handling payment events, including validation and status updates

---
+++ apps/payment-service/src/application/services/payment-event.service.ts
@@ -0,0 +1,60 @@
+import type { PaymentRepository, PaymentEventRepository } from '../';
+import type { PaymentEvent } from '../../domain';
+
+import { NotFoundError, ConflictError } from '@workspace/errors';
+
+const eventStatusDependencies = {
+    'payment-succeeded': ['processing'],
+    'payment-processing': ['initiated'],
+    'payment-failed': ['processing'],
+    'payment-canceled': ['initiated'],
+};
+
+class PaymentEventService {
+    constructor(
+        private readonly paymentRepository: PaymentRepository,
+        private readonly paymentEventRepository: PaymentEventRepository,
+    ) {}
+
+    async handlePaymentEvent(paymentEvent: PaymentEvent): Promise<void> {
+        if (paymentEvent.event === 'payment-initiated') {
+            return;
+        }
+
+        const existingPaymentEvent = await this.paymentEventRepository.findPaymentEventByIdempotencyKey(paymentEvent.idempotencyKey);
+
+        if (existingPaymentEvent) {
+            throw new ConflictError(`Payment event with idempotency key ${paymentEvent.idempotencyKey} already exists`);
+        }
+
+        await this.paymentEventRepository.createPaymentEvent(paymentEvent);
+
+        const payment = await this.paymentRepository.getPaymentByProviderPaymentId(
+            paymentEvent.providerPaymentId,
+            paymentEvent.provider,
+        );
+
+        if (!payment) {
+            throw new NotFoundError(`Payment with provider payment id ${paymentEvent.providerPaymentId} not found`);
+        }
+
+        if (paymentEvent.event === 'payment-succeeded' && payment.status === `${paymentEvent.status}`) {
+            return;
+        }
+
+        if (eventStatusDependencies[paymentEvent.event].includes(payment.status)) {
+            throw new ConflictError(
+                `Cannot set payment status to "${paymentEvent.status}" because payment is not in "${eventStatusDependencies[paymentEvent.event].join(', ').trim()}" statuses`,
+            );
+        }
+
+        await this.paymentRepository.updatePaymentStatusByProviderPaymentId(
+            paymentEvent.providerPaymentId,
+            paymentEvent.status,
+            paymentEvent.provider,
+        );
+    }
+}
+
+export default PaymentEventService;
+export { PaymentEventService };
